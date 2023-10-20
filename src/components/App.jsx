88ea7c22c42b08247891c54d087723c30bd6fe36
kaonashiiiiiii <priora102003@gmail.com>
2023-10-20 14:48:07 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,34 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { Component  } from "react";
 import { ContactForm, FilterForm, Section, ContactList } from "."
 
-export const App = () => {
-  const [state, setState] = useState({
+export class App extends Component {
+  state = {
     contacts: [],
-    filter: ''
-  })
-
-  const rendersCausedByContacts = useRef(0)
-
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts')
-    if (contacts) {
-      setState((prevState) => ({
-        ...prevState,
-        contacts: JSON.parse(contacts)
-      }))
-    }
-  }, [])
-
-  useEffect(() => {
-    // to skip setting empty array on initial render
-    if (rendersCausedByContacts.current === 0) {
-      rendersCausedByContacts.current += 1
-      return
-    }
-
-    localStorage.setItem('contacts', JSON.stringify(state.contacts))
-  }, [state.contacts])
-
-  function deleteContact (id) {
-    setState(prevState => ({
-      ...prevState,
-      contacts: prevState.contacts.filter(item => item.id !== id)
-    }))
-    localStorage.setItem('contacts', JSON.stringify(state.contacts))
+    filter: '',
   }
 
-  function checkContact (contact) {
-    const isContantExist = state.contacts.find(item => item.name === contact.name)
+  componentDidMount () {
+    const inititalContacts = localStorage.getItem('contacts')
+    this.setState({
+      contacts: JSON.parse(inititalContacts)
+    })
+  }
+
+  componentDidUpdate(_, prevState) {
+    console.log('prevState', prevState);
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  deleteContact = (id) => {
+    this.setState({
+      contacts: this.state.contacts.filter(item => item.id !== id)
+    })
+  }
+
+  checkContact = (contact) => {
+    const isContantExist = this.state.contacts.find(item => item.name === contact.name)
     if (isContantExist) {
       alert (`${contact.name} is already in contacts`)
       return false
@@ -46,39 +36,39 @@ export const App = () => {
     return true
   }
 
-  function addContact (contact) {
-    if (!checkContact(contact)) return
-    setState(prevState => ({
-      ...prevState,
-      contacts: [...prevState.contacts, contact]
-    }))
-    localStorage.setItem('contacts', JSON.stringify(state.contacts))
+  addContact = (contact) => {
+    if (!this.checkContact(contact)) return
+    this.setState({
+      contacts: [...this.state.contacts, contact]
+    })
   }
 
-  const filteredContacts = useMemo(function () {
-    return state.contacts.filter(contact => {
-      if(!state.filter) return true
-      if (contact.name.toLowerCase().includes(state.filter)) return true
+  getFilteredContacts = () => {
+    return this.state.contacts.filter(contact => {
+      if(!this.state.filter) return true
+      if (contact.name.toLowerCase().includes(this.state.filter)) return true
       return false
     })
-  }, [state.filter, state.contacts])
-
-  function setFilter (filter) {
-    setState(prevState => ({
-      ...prevState,
-      filter
-    }))
   }
-  console.log(filteredContacts)
-  return (
-    <div>
+
+  setFilter = (filter) => {
+    this.setState({
+      filter
+    })
+  }
+  
+  render () {
+    const filteredContacts = this.getFilteredContacts()
+    return (
+      <div>
       <Section title="Phonebook">
-        <ContactForm addContact={addContact}/>
+        <ContactForm addContact={this.addContact}/>
       </Section>
       <Section title="Contacts">
-        <FilterForm filter={state.filter} setFilter={setFilter}/>
-        <ContactList contacts={filteredContacts} deleteContact={deleteContact}/>
+        <FilterForm filter={this.state.filter} setFilter={this.setFilter}/>
+        <ContactList contacts={filteredContacts} deleteContact={this.deleteContact}/>
       </Section>
     </div>
-  );
+    )
+  }
 };
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,39 +1,52 @@
-import React, { useState } from 'react'
+import { Component } from 'react'
 import styles from './contactForm.module.css'
 
-const ContactForm = ({ addContact}) => {
-  const [name, setName] = useState('')
-  const [number, setNumber] = useState('')
-
-  function resetForm () {
-    setName('')
-    setNumber('')
+class ContactForm extends Component  {
+  state = {
+    name: '',
+    number: ''
   }
 
-  function onAddContactClick () {
-    if (!name || !number) return
+  resetForm = () => {
+    this.setState({
+      name: '',
+      number: ''
+    })
+  }
+
+  onAddContactClick = () => {
+    if (!this.state.name || !this.state.number) return
     const contact = {
       id: crypto.randomUUID(),
-      name,
-      number,
+      name: this.state.name,
+      number: this.state.number,
     }
-    addContact(contact)
-    resetForm()
+    this.props.addContact(contact)
+    this.resetForm()
   }
-  return (
-    <form className={styles['contact-form']}>
+
+  onChange = (e, property) => {
+    this.setState({
+      [property]: e.target.value
+    })
+  }
+
+  render () {
+    return (
+      <form className={styles['contact-form']}>
       <div>
         <label htmlFor="name">Name</label>
-        <input value={name} type="text" name="name" required onChange={(e) => setName(e.target.value)}/>
+        <input value={this.state.name} type="text" name="name" required onChange={(e) => this.onChange(e, 'name')}/>
       </div>
       <div>
         <label htmlFor="number">Number</label>
-        <input value={number} type="tel" name="number" required onChange={(e) => setNumber(e.target.value)}/>
+        <input value={this.state.number} type="tel" name="number" required onChange={(e) => this.onChange(e, 'number')}/>
       </div>
 
-      <button className={styles['contact-button']} type="button" onClick={onAddContactClick}>Add</button>
+      <button className={styles['contact-button']} type="button" onClick={this.onAddContactClick}>Add</button>
     </form>
-  )
+    )
+  }
 }
 
 export default ContactForm
